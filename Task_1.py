326c4cc901fa4b3683956783e09778b44b62b8c8
Loreanna <s.s.khovanskaya@gmail.com>
2022-08-18 00:46:52 +0300

    Finished

--- Task_1.py
+++ Task_1.py
@@ -1,18 +1,20 @@
 import requests
 
-list_of_heroes = ["Hulk", "Captain America", "Thanos"]
+def determine_intelligent_hero(list_of_heroes):
+    url = "https://akabab.github.io/superhero-api/api/all.json"
+    resp = requests.get(url)
+    total_info = resp.json()
+    intelligence = 0
+    most_intelligent_hero = ""
+    for hero in total_info:
+        if hero["name"] in list_of_heroes:
+            if hero["powerstats"]["intelligence"] > intelligence:
+                intelligence = hero["powerstats"]["intelligence"]
+                most_intelligent_hero = hero["name"]
 
-url = "https://akabab.github.io/superhero-api/api/all.json"
-resp = requests.get(url)
-total_info = resp.json()
+    print(f'Самый умный супергерой - {most_intelligent_hero}, с интеллектом = {intelligence}.')
+    return
 
-intelligence = 0
-most_intelligent_hero = ""
-for hero in total_info:
-    if hero["name"] in list_of_heroes:
-        if hero["powerstats"]["intelligence"] > intelligence:
-            intelligence = hero["powerstats"]["intelligence"]
-            most_intelligent_hero = hero["name"]
-
-print(f'Самый умный супергерой - {most_intelligent_hero}, с интеллектом = {intelligence}.')
-
+if __name__ == '__main__':
+    heroes_list = ["Hulk", "Captain America", "Thanos"]
+    determine_intelligent_hero(heroes_list)
